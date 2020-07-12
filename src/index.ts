b9138fdf1c0ce46704da377dd234bb39f33b43d1
ohunyk <ohunyk@gmail.com>
2020-07-12 21:47:41 +0900

    Typescript class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-const name = "Kwon",
-  age = 30,
-  gender = "male";
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
 
-const sayHi = (name: string, age: number, gender?: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const min = new Human("Min", 20, "female");
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(name, 444, "male"));
+console.log(sayHi(min));
 
 export {};
